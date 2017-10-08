b446b4294a40b179d8317a146578890befb7fbbd
cnov20 <128@holbertonschool.com>
2017-10-07 21:15:47 -0700

    Fixed routes, added app file and instatntiation - setup server to serve files using express / node modules

---
+++ app.js
@@ -0,0 +1,21 @@
+// Dependencies from node_modules folder created with npm init
+const path = require('path');
+const express = require('express');
+
+// App Instantiation
+const app = express();
+
+app.engine('html', require('ejs').renderFile);
+
+app.use(express.static('public'));
+
+//app.use(express.static(path.join(__dirname, '/assets')));
+//app.use(express.static(path.join(__dirname, '/css')));
+
+app.get('/', function (req, res) {
+  res.render('index.html');
+});
+
+app.listen(5000, function() {
+  console.log('listening');
+});
